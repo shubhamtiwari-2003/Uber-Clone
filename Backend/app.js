a5f1f04c8d585eac24a6f97eefd3f3e2b6aee4a0
shubham-1118 <shubhamtiwari_21521@aitpune.edu.in>
2024-12-01 03:33:24 +0530

    New Changes

--- Backend/app.js
+++ Backend/app.js
@@ -3,17 +3,24 @@ dotenv.config();
 const express = require("express");
 const cors = require('cors');
 const app  = express();
+const userRoutes = require('./routes/user.routes');
 
+
+const connectToDb = require("./db/db");
+
+connectToDb();
 
 app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 
 app.get("/",(req,res)=>{
     res.send('Hello World!');
 });
 
+app.use('/users', userRoutes);
 
-// app.listen(3000, console.log("Server Started on port ", 3000));
 
 
 module.exports = app; 
---
+++ Backend/models/user.model.js
@@ -0,0 +1,58 @@
+const mongoose = require("mongoose");
+
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+
+
+const userSchema = new mongoose.Schema( {
+    fullname:{
+        firstname:{
+            type: String,
+            required:true,
+            minlength:[3,'First name should be at least 3 characters long'],
+        },
+        lastname:{
+            type: String,
+            minlength:[3,'First name should be at least 3 characters long'],
+        }
+    },
+    email:{
+        type: String,
+        required : true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+    socketId:{
+        type: String,
+    },
+
+})
+
+
+//method function to create JWT token
+userSchema.methods.generateAuthToken = function(){
+    const token = jwt.sign({ _id: this._id}, process.env.JWT_SECRET)
+    return token;
+}
+
+
+// method function to compare password
+userSchema.methods.comparePasssword = async function(password){
+    return await bcrypt.compare(password,this.password);
+}
+
+
+//fucntion to hash the password
+userSchema.statics.hashPassword = async function (password) {
+    return await bcrypt.hash(password, 10);
+}
+
+
+const userModel = mongoose.model('user', userSchema);
+
+module.exports = userModel;
